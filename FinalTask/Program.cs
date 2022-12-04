9cc802b79164553d903e4dee54963f8caddc793d
Nastya <anpchernova@mail.ru>
2022-12-05 01:01:13 +0300

    FinalTask

---
+++ FinalTask/Program.cs
@@ -0,0 +1,30 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+
+void SecondStringArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.Clear();
+string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+string[] array2 = new string[array1.Length];
+PrintArray(array1);
+SecondStringArray(array1, array2);
+PrintArray(array2);
